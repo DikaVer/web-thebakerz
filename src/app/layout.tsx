765615767676473a84acb1116a6a5fe18838a478
dikaver <dika1596@gmail.com>
2024-12-03 07:16:30 +0100

    Speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import {Toaster} from "@/components/ui/sonner";
 import {metadataDefault} from "@/components/metadata";
 import {Providers} from "@/app/providers";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 export const metadata: Metadata = metadataDefault;
@@ -25,6 +26,7 @@ export default function RootLayout({
                             {children}
                             <Toaster/>
                 </Providers>
+                <SpeedInsights />
             </body>
             </html>
     );
