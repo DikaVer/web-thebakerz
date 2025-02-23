bcb4d4de07967b62a59b8ff56dbe951a792c7f38
dikaver <dika1596@gmail.com>
2025-02-23 06:01:21 +0100

    fix working hours

--- src/components/store/store-header/subheader/working-hours.tsx
+++ src/components/store/store-header/subheader/working-hours.tsx
@@ -31,67 +31,16 @@ export const renderCalendarTopContent = () => {
 
     const { store } = useStore();
 
-    const isSmall = useMediaQuery("(max-width: 768px)");
+    const isSmall = useMediaQuery("(max-width: 767px)");
 
     if (!store?.schedule) {
         return <div className="w-full mx-2 max-w-52 text-default-500 text-center">No schedule available</div>;
     }
 
-    return isSmall ? renderWorkingHoursDropdown({ store }) : renderWorkingHoursTooltip({ store });
+    return renderWorkingHoursDropdown({ store }) ;
 
 };
 
-export const renderWorkingHoursTooltip = ({store} : {store: StoreData}) => {
-    return (
-        <Tooltip
-            content={
-                <div className="flex flex-wrap items-center justify-center max-w-52">
-                    {["monday", "friday", "tuesday", "thursday", "wednesday", "saturday", "sunday"].map((day, index) => {
-                        //@ts-ignore
-                        const workday = store.schedule[day];
-
-                        const shortDay = getShortWeekday(day);
-                        let displayText = `Closed`;
-                        if (workday && (workday as WorkDay).isEnabled) {
-                            const wd = workday as WorkDay;
-                            const startHour = pad(wd.start.hour);
-                            const startMinute = pad(wd.start.minute);
-                            const endHour = pad(wd.end.hour);
-                            const endMinute = pad(wd.end.minute);
-                            displayText = `${startHour}:${startMinute} - ${endHour}:${endMinute}`;
-                        }
-
-                        const isSunday = day === "sunday";
-
-                        return (
-                            <div
-                                key={day}
-                                className={`${isSunday ? "ml-4 w-[40%] text-start" : `w-1/2 ${index % 2 == 0 ? 'text-start' : 'text-end'} `} flex flex-col`}
-                            >
-                                <span className="text-sm font-medium text-default-600">
-                                  {day.charAt(0).toUpperCase() + day.slice(1)}
-                                </span>
-                                <p className="text-default-500 text-xs font-light">
-                                    {displayText}
-                                </p>
-                            </div>
-                        );
-                    })}
-                </div>
-            }
-        >
-                <Button
-                    size={"sm"}
-                    variant="bordered"
-                    radius={'lg'}
-                    className={'text-default-600 bg-gradient-card w-full'}
-                    startContent={ <Icon icon={"solar:sort-by-time-linear"} width={24} className={"text-default-500"}/>}
-                >
-                    View Schedule
-                </Button>
-        </Tooltip>
-    );
-}
 
 export const renderWorkingHoursDropdown = ({store} : {store: StoreData}) => {
     return (
@@ -105,12 +54,54 @@ export const renderWorkingHoursDropdown = ({store} : {store: StoreData}) => {
                     size={"sm"}
                     variant="bordered"
                     radius={'full'}
-                    className={'text-default-600 bg-gradient-card w-full'}
-                    startContent={ <Icon icon={"solar:sort-by-time-linear"} width={24} className={"text-default-500"}/>}
+                    className={'text-default-600 bg-gradient-card'}
                 >
-                    <p className={'w-[90%] truncate'}>
-                        Working Hours
-                    </p>
+                    <Tooltip
+                        offset={15}
+                        content={
+                            <div className="flex flex-wrap items-center justify-center max-w-52">
+                                {["monday", "friday", "tuesday", "thursday", "wednesday", "saturday", "sunday"].map((day, index) => {
+                                    //@ts-ignore
+                                    const workday = store.schedule[day];
+
+                                    const shortDay = getShortWeekday(day);
+                                    let displayText = `Closed`;
+                                    if (workday && (workday as WorkDay).isEnabled) {
+                                        const wd = workday as WorkDay;
+                                        const startHour = pad(wd.start.hour);
+                                        const startMinute = pad(wd.start.minute);
+                                        const endHour = pad(wd.end.hour);
+                                        const endMinute = pad(wd.end.minute);
+                                        displayText = `${startHour}:${startMinute} - ${endHour}:${endMinute}`;
+                                    }
+
+                                    const isSunday = day === "sunday";
+
+                                    return (
+                                        <div
+                                            key={day}
+                                            className={`${isSunday ? "ml-4 w-[40%] text-start" : `w-1/2 ${index % 2 == 0 ? 'text-start' : 'text-end'} `} flex flex-col`}
+                                        >
+                                        <span className="text-sm font-medium text-default-600">
+                                          {day.charAt(0).toUpperCase() + day.slice(1)}
+                                        </span>
+                                            <p className="text-default-500 text-xs font-light">
+                                                {displayText}
+                                            </p>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        }
+                    >
+                        <div className={'flex items-center gap-x-2 w-full'}>
+                            <Icon icon={"solar:sort-by-time-linear"} width={24}
+                                  className={"text-default-500"}/>
+                            <p className={'w-[90%] truncate'}>
+                                Working Hours
+                            </p>
+                        </div>
+                    </Tooltip>
                 </Button>
             </DropdownTrigger>
             <DropdownMenu
