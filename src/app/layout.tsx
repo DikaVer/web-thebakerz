c6d0be329928aff4a0ee688337cf28a02644c395
DikaVer <72268276+DikaVer@users.noreply.github.com>
2025-02-23 21:40:04 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ import {getCurrentSession} from "@/lib/actions/session";
 export const viewport: Viewport = {
     width: 'device-width',
     initialScale: 1,
-    maximumScale: 10,
+    maximumScale: 1,
     userScalable: true,
 }
 
