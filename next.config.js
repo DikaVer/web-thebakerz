fbc1212c50582c18f246efe2a717ab0efed88a53
dikaver <dika1596@gmail.com>
2025-03-27 17:10:47 +0100

    increase api limits

--- next.config.js
+++ next.config.js
@@ -17,6 +17,9 @@ const nextConfig = {
                 },
             },
         },
+    serverActions: {
+        bodySizeLimit: '5mb',
+        },
     },
     bundlePagesRouterDependencies: true,
     eslint: {
