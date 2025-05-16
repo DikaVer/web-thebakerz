a22ede2cdf6e8bcb7f9930d90835f6f2c81ac019
DikaVer <dika1596@gmail.com>
2025-05-16 12:10:47 +0200

    fix templates

--- src/lib/actions/product.ts
+++ src/lib/actions/product.ts
@@ -279,7 +279,7 @@ export async function getProductByStoreIdAndWebName(storeId: string, webName: st
         console.log("storeId", storeId);
 
         const querySpec = {
-            query: "SELECT c.id, c.store_id, c.store_name, c.web_name, c.category, c.name, c.description, c.price, c.picture, c.ingredients, c.allergies, c.dietary, c.constId, c.additionalImages, c.variants, c.min_order, c.min_lead_time, c.hide_product FROM c WHERE c.store_id = @storeId AND (c.web_name = @webName OR c.constId = @webName) AND c.archive = false",
+            query: "SELECT c.id, c.store_id, c.store_name, c.web_name, c.category, c.name, c.description, c.price, c.picture, c.ingredients, c.allergies, c.dietary, c.constId, c.additionalImages, c.variants, c.min_order, c.min_lead_time, c.hide_product FROM c WHERE c.store_id = @storeId AND (c.web_name = @webName OR c.id = @webName) AND c.archive = false",
             parameters: [
                 { name: "@storeId", value: storeId },
                 { name: "@webName", value: webName }
@@ -319,7 +319,7 @@ export async function getCurrentProduct(storeId: string, productId: string): Pro
             },
             next: {
                 tags: ['products'],
-                revalidate: 300
+                revalidate: 0
             }
         }).then(res => res.json());
 
