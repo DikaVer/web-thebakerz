32baacd57904468304ba785406e140403b18ff2e
DikaVer <dika1596@gmail.com>
2025-06-05 21:38:44 +0200

    fix products

--- src/lib/actions/product.ts
+++ src/lib/actions/product.ts
@@ -218,7 +218,7 @@ export async function getProductByStoreIdAndWebName(storeId: string, webName: st
         console.log("storeId", storeId);
 
         const querySpec = {
-            query: "SELECT c.id, c.store_id, c.web_name, c.category, c.name, c.description, c.price, c.picture, c.ingredients, c.allergies, c.dietary, c.constId, c.additionalImages, c.variants, c.min_order, c.min_lead_time, c.hide_product, c.isPostDelivery FROM c WHERE c.store_id = @storeId AND (c.web_name = @webName OR c.id = @webName) AND c.archive = false",
+            query: "SELECT c.id, c.store_id, c.web_name, c.category, c.name, c.description, c.price, c.picture, c.ingredients, c.allergies, c.dietary, c.constId, c.additionalImages, c.variants, c.min_order, c.min_lead_time, c.hide_product, c.isPostDelivery FROM c WHERE c.store_id = @storeId AND (c.web_name = @webName OR c.constId = @webName) AND c.archive = false",
             parameters: [
                 { name: "@storeId", value: storeId },
                 { name: "@webName", value: webName }
