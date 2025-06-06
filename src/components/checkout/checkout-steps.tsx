54c743e09b7a1692bba28adba88ef2bd800f26d8
DikaVer <dika1596@gmail.com>
2025-06-06 03:27:24 +0200

    fast checkout

--- src/components/checkout/checkout-steps.tsx
+++ src/components/checkout/checkout-steps.tsx
@@ -13,10 +13,13 @@ import showErrorMessage from "@/components/toast/toast-error";
 import { useTranslations } from "next-intl";
 import { replaceGuestAddress } from "@/lib/actions/delivery-actions";
 import clarity from "@microsoft/clarity";
+import { useDelivery } from "@/components/providers/delivery-provider";
+import { useRouter } from "next/navigation";
 
 export default function CheckoutSteps({ }: {}) {
     const { session } = useSession();
     const t = useTranslations("app/(store)/components/checkout-steps");
+    const router = useRouter();
     useEffect(() => {
         clarity.upgrade("checkout");
         clarity.setTag("page", "checkout-steps");
@@ -24,9 +27,28 @@ export default function CheckoutSteps({ }: {}) {
 
     const { store } = useStore();
     const steps = ["1", "2", "3", "4"];
-    const [currentStep, setCurrentStep] = useState<number>(session?.user ? 2 : 1);
+    const { isDelivery, selectedDate, validationResult } = useDelivery();
+    const canProceedToPayment = isDelivery 
+        ? validationResult.isValid && validationResult.isInRange && selectedDate
+        : selectedDate;
+
+    // const initialStep = session?.user 
+    //     ? canProceedToPayment ? 3 : 2 
+    //     : 2;
+
+    const initialStep = canProceedToPayment ? 3 : 2 
+    
+
+    const [currentStep, setCurrentStep] = useState<number>(initialStep);
     const disabledKeys = steps.filter((key) => Number(key) > currentStep);
     const [selectedKey, setSelectedKey] = useState<string>(currentStep.toString());
+    const storeUrl = store?.storeName ? store?.storeName : store?.id;
+
+    // useEffect(() => {
+    //     if (canProceedToPayment) {
+    //         router.push(`/${storeUrl}/pay`);
+    //     }
+    // }, [canProceedToPayment]);
 
     if (session?.user?.role === "bakerz" && session?.user?.id === store.user_id) {
         return NotFound();
