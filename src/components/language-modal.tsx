482d8359d69cd7a51ffa25443c5163c1757c8628
DikaVer <dika1596@gmail.com>
2025-06-10 20:55:04 +0200

    improve translations

--- src/components/language-modal.tsx
+++ src/components/language-modal.tsx
@@ -58,7 +58,7 @@ export default function LanguageModal({handAction}: {handAction?: () => void}) {
                         aria-label="Select Dutch"
                         variant={'light'}
                         size={'lg'}
-                        className={'justify-start px-0 data-[hover=true]:bg-background data-[hover=true]:text-lg hidden'}
+                        className={'justify-start px-0 data-[hover=true]:bg-background data-[hover=true]:text-lg'}
                         startContent={<div className={'w-8'}><Icon icon="circle-flags:lang-nl" width={32}/></div>}
                         onPress={(e) => {
                             handleSubmit('nl')
@@ -100,6 +100,21 @@ export default function LanguageModal({handAction}: {handAction?: () => void}) {
                         </div>
                     </Button>
                     <Button
+                        aria-label="Select Spanish"
+                        variant={'light'}
+                        size={'lg'}
+                        className={'justify-start px-0 data-[hover=true]:bg-background data-[hover=true]:text-lg'}
+                        startContent={<div className={'w-8'}><Icon icon="circle-flags:lang-es" width={32}/></div>}
+                        onPress={(e) => {
+                            handleSubmit('es')
+                        }}
+                    >
+                        <div className={'flex flex-col items-start'}>
+                            <p>España</p>
+                            <p className={'text-xs text-default-500 truncate'}>Puede contener errores de traducción</p>
+                        </div>
+                    </Button>
+                    <Button
                         aria-label="Select Russian"
                         variant={'light'}
                         size={'lg'}
@@ -118,7 +133,7 @@ export default function LanguageModal({handAction}: {handAction?: () => void}) {
                         aria-label="Select Ukrainian"
                         variant={'light'}
                         size={'lg'}
-                        className={'hidden justify-start px-0 data-[hover=true]:bg-background data-[hover=true]:text-lg'}
+                        className={'justify-start px-0 data-[hover=true]:bg-background data-[hover=true]:text-lg'}
                         startContent={<div className={'w-8'}><Icon icon="circle-flags:lang-uk" width={32}/></div>}
                         onPress={(e) => {
                             handleSubmit('uk')
